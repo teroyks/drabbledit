83a48b8524a263db6fd4dd64d5eae1c98ee37b1d
Tero Ykspetäjä <tero.ykspetaja@tact.fi>
2020-02-02 12:40:35 +0200

    Replace state machine with simple reactivity
    Only restoring the default title on page load and when leaving the field
    (and not immediately after deleting the title text) removed the need
    to handle events (because the same event never removes and restores the
    default text so there is no conflict).

--- src/EditableTitle.svelte
+++ src/EditableTitle.svelte
@@ -7,123 +7,25 @@
 
   const defaultTitle = "Title: A Drabble";
 
-  const State = {
-    NotUpdated: "Not updated",
-    Updated: "Updated",
-    Deleted: "Deleted",
-    Pending: "Pending"
-  };
-
-  const Action = {
-    Blur: "Blur",
-    Key: "Key", // key press
-    InterruptClear: "Interrupt - Clear" // automatic interruption
-  };
-
-  // current state of the title
-  let currentState;
-
-  // action performed on the title element
-  let uiAction;
-
   // title DOM element
   let element;
 
   // title text
   let title = localStorage.getItem("title") || defaultTitle;
-  // keep tabs on whether title was updated to avoid
-  // unnecessary actions (on control keys etc.)
-  let oldTitle;
 
-  // handle title action when has a value
-  $: uiAction && handleAction();
-  $: updateTitle(title);
+  $: localStorage.setItem("title", title);
 
-  const saveTitle = () => localStorage.setItem("title", title);
-
-  const titleChanged = () => title !== oldTitle;
   const removeDefaultTitle = () => {
-    uiAction = Action.InterruptClear;
-    currentState = State.Pending;
-    title = "";
+    if (title === defaultTitle) title = "";
   };
 
-  const emitKey = () => (uiAction = Action.Key);
-  const emitBlur = () => (uiAction = Action.Blur);
+  const restoreDefaultTitleIfEmpty = () => (title = title || defaultTitle);
 
-  const handleAction = () => {
-    // console.table({
-    //   method: "handleAction",
-    //   uiAction,
-    //   currentState,
-    //   title
-    // });
-    switch (uiAction) {
-      case Action.Blur:
-        if (!title) title = defaultTitle;
-        break;
-      case Action.InterruptClear:
-        break;
-      case Action.Key:
-        if (!currentState === State.Pending) oldTitle = title;
-        switch (currentState) {
-          case State.NotUpdated:
-          case State.Deleted:
-            // console.debug("check page load situation");
-            if (title === defaultTitle) {
-              // console.debug("remove default title");
-              removeDefaultTitle();
-            }
-            break;
-          case State.Updated:
-            break;
-          default:
-            console.error(`unhandled currentState: ${currentState}`);
-        }
-        break;
-    }
-  };
-
-  const updateTitle = _ => {
-    // console.table({
-    //   method: "updateTitle",
-    //   uiAction,
-    //   currentState,
-    //   title
-    // });
-    const action = uiAction;
-    if (currentState !== State.Pending) uiAction = null; // reset
-    switch (action) {
-      case Action.InterruptClear:
-        // console.debug("automatically triggered update");
-        currentState = State.Pending;
-        saveTitle();
-        uiAction = Action.Key;
-        break;
-      case Action.Key:
-        // console.debug("...by key pressed");
-        if (titleChanged()) {
-          // console.debug("title changed:", title);
-          currentState = title ? State.Updated : State.Deleted;
-          saveTitle();
-        }
-        break;
-      case Action.Blur:
-        break;
-      case undefined:
-      case null:
-        // console.debug("no user edit");
-        if (!title) console.debug("title is empty");
-        break;
-      default:
-        console.error("Unknown action:", action);
-    }
-    // console.debug("State after update:", currentState);
-  };
+  const onKeyPress = () => removeDefaultTitle();
+  const onBlur = () => restoreDefaultTitleIfEmpty();
 
   onMount(() => {
     element.focus();
-    currentState = State.NotUpdated;
   });
 </script>
 
@@ -140,7 +42,7 @@
   contenteditable="true"
   bind:textContent={title}
   bind:this={element}
-  on:keydown={emitKey}
-  on:blur={emitBlur}>
+  on:keypress={onKeyPress}
+  on:blur={onBlur}>
   {defaultTitle}
 </h1>
