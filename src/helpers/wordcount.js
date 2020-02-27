5f86e51dbd5c8b01cf32291a25fe8bb9563aab92
Tero Ykspetäjä <tero.ykspetaja@tact.fi>
2020-02-27 14:54:39 +0200

    💡 Add JSDoc to wordcount functions

--- src/helpers/wordcount.js
+++ src/helpers/wordcount.js
@@ -1,5 +1,13 @@
+/**
+ * @param {string} str 
+ * @returns {boolean}
+ */
 const ignoreNonWords = str => str.match(/\w/i)
 
+/**
+ * @param {string} string 
+ * @returns {int}
+ */
 const countWords = (string) => string ?
   string
     .trim()
